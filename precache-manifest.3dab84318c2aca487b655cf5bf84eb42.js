6a947395acb877bd5434ca1dcc8d763f073f6654
Christopher Carrasco <CarrascoC81@gmail.com>
2020-08-06 15:10:46 -0400

    Updates

---
+++ precache-manifest.3dab84318c2aca487b655cf5bf84eb42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "099a5443656cf657900798305d90696e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17d5fa5da8a806620f4f",
+    "url": "/static/css/main.5950d668.chunk.css"
+  },
+  {
+    "revision": "390b807730df09e9026a",
+    "url": "/static/js/2.0e4a70b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0e4a70b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17d5fa5da8a806620f4f",
+    "url": "/static/js/main.83737985.chunk.js"
+  },
+  {
+    "revision": "75c08e746148008b83ea",
+    "url": "/static/js/runtime-main.510739d2.js"
+  },
+  {
+    "revision": "86777de5bb34f5f054ac51bdce9d402e",
+    "url": "/static/media/logo.86777de5.svg"
+  }
+]);
